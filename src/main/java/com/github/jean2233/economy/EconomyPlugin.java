3c2ca85e89baf34f38732c01780764e671ad63f9
Jean2233 <jeanbassan2018@hotmail.com>
2022-11-10 12:39:39 -0300

    first commit

---
+++ src/main/java/com/github/jean2233/economy/EconomyPlugin.java
@@ -0,0 +1,11 @@
+package com.github.jean2233.economy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class EconomyPlugin extends JavaPlugin {
+    @Override
+    public void onEnable() {}
+
+    @Override
+    public void onDisable() {}
+}
